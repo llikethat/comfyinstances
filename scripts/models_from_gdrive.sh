1d72d0507fb754f408fa0945c929d87aba339c9f
llikethat <llikethat@users.noreply.github.com>
2025-08-27 18:30:37 +0530

    script to download models from gDrive
    script to download models from gDrive

---
+++ scripts/models_from_gdrive.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+source /venv/main/bin/activate
+COMFYUI_DIR=${WORKSPACE}/ComfyUI
+
+APT_PACKAGES=(
+    #"package-1"
+    #"package-2"
+)
+
+PIP_PACKAGES=(
+    #"package-1"
+    #"package-2"
+)
+
+NODES=(
+    #"https://github.com/ltdrdata/ComfyUI-Manager"
+    #"https://github.com/cubiq/ComfyUI_essentials"
+)
+
+WORKFLOWS=(
+    # Example Google Drive link (replace with your own)
+    "https://drive.google.com/uc?id=FILE_ID_1"
+    # Add more Google Drive links as needed
+)
+
+CLIP_MODELS=(
+    # Example Google Drive link (replace with your own)
+    "https://drive.google.com/uc?id=FILE_ID_2"
+    # Add more Google Drive links as needed
+)
+
+UNET_MODELS=(
+    # Example Google Drive link (replace with your own)
+    "https://drive.google.com/file/d/169duNM6qbYToNwwBalU7ovujc2PZLa_0/view?usp=drive_link"
+)
+
+VAE_MODELS=(
+    # Example Google Drive link (replace with your own)
+    "https://drive.google.com/uc?id=FILE_ID_4"
+)
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
+
+function provisioning_start() {
+    provisioning_print_header
+    provisioning_get_apt_packages
+    provisioning_get_nodes
+    provisioning_get_pip_packages
+    workflows_dir="${COMFYUI_DIR}/user/default/workflows"
+    mkdir -p "${workflows_dir}"
+    provisioning_get_files "${workflows_dir}" "${WORKFLOWS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/unet" "${UNET_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/vae"  "${VAE_MODELS[@]}"
+    provisioning_get_files "${COMFYUI_DIR}/models/clip" "${CLIP_MODELS[@]}"
+    provisioning_print_end
+}
+
+function provisioning_get_apt_packages() {
+    if [[ -n $APT_PACKAGES ]]; then
+        sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ -n $PIP_PACKAGES ]]; then
+        pip install --no-cache-dir ${PIP_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="${COMFYUI_DIR}custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                   pip install --no-cache-dir -r "$requirements"
+                fi
+            fi
+        else
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+            if [[ -e $requirements ]]; then
+                pip install --no-cache-dir -r "${requirements}"
+            fi
+        fi
+    done
+}
+
+function provisioning_get_files() {
+    if [[ -z $2 ]]; then return 1; fi
+
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    arr=("$@")
+    printf "Downloading %s file(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
+    done
+}
+
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete:  Application will start now\n\n"
+}
+
+# Download from $1 URL to $2 file path
+function provisioning_download() {
+    # If it's a Google Drive link, use gdown
+    if [[ $1 == *"drive.google.com"* ]]; then
+        file_id=$(echo "$1" | grep -o 'id=[^&]*' | cut -d'=' -f2)
+        if [ -n "$file_id" ]; then
+            # Ensure gdown is installed
+            if ! command -v gdown &> /dev/null; then
+                pip install gdown
+            fi
+            gdown --id "$file_id" -O "$2"
+        else
+            # If the link is in the format /file/d/FILE_ID
+            file_id=$(echo "$1" | grep -o '/d/[^/]*' | cut -d'/' -f3)
+            if [ -n "$file_id" ]; then
+                if ! command -v gdown &> /dev/null; then
+                    pip install gdown
+                fi
+                gdown --id "$file_id" -O "$2"
+            else
+                echo "Invalid Google Drive link format: $1"
+            fi
+        fi
+    else
+        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+    fi
+}
+
+if [[ ! -f /.noprovisioning ]]; then
+    provisioning_start
+fi
