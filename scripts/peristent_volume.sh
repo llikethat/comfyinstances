cf8fd36097ba6dcd18750be745ab6db7812442f4
llikethat <llikethat@users.noreply.github.com>
2025-09-20 13:35:37 +0530

    Update peristent_volume.sh

--- scripts/peristent_volume.sh
+++ scripts/peristent_volume.sh
@@ -222,8 +222,10 @@ function provisioning_download() {
         auth_token="$CIVITAI_TOKEN"
     fi
     if [[ -n $auth_token ]];then
+        echo "wget --header=\"Authorization: Bearer $auth_token\" -nc --content-disposition --show-progress -e dotbytes=\"${3:-4M}\" -P \"$2\" \"$1\""
         wget --header="Authorization: Bearer $auth_token" -nc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
     else
+        echo "wget -nc --content-disposition --show-progress -e dotbytes=\"${3:-4M}\" -P \"$2\" \"$1\""
         wget -nc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
     fi
 }
